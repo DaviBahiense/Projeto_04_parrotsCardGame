6eab5b93d9baa5d9174d4ab1665d4e7220077f93
Davi Bahiense <davibnovaes@peb.ufrj.br>
2021-11-06 01:56:30 -0300

    automatizando cartas

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,29 @@
 let qtdCard
 
-function pedirPar ()  {
-    
-    qtdCard = prompt ("Insira uma quantidade de cartas a jogar. O valor precisa ser par");
-    while (qtdCard % 2 !== 0 ) {
-     return pedirPar()
-    }
-    
+
+qtdCard = prompt ("Insira uma quantidade de cartas entre 4 e 14. O valor precisa ser par");
+
+while(qtdCard % 2 !== 0 || qtdCard < 4 || qtdCard > 14) {
+    qtdCard = prompt ("Insira uma quantidade de cartas entre 4 e 14. O valor precisa ser par");
 }
-//pedirPar()
+
+for ( let i = 0; i < qtdCard; i++) {
+    let content = document.querySelector(".content")
+    content.innerHTML += `
+    <div class="card" onclick="turn(this)">
+            <div class="face front-face">
+                <img class="parrot" src="./image/image_front/front.png" alt="">  
+            </div>
+            <div class="face back-face">
+                <img class="gif" src="./image/image_back/bobrossparrot.gif" alt="">
+            </div>
+        </div>
+        `
+}
+
 
 function turn(button) {
     button.classList.toggle("card-select")
+    
 }
 
